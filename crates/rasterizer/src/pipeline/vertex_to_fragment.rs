f88599745c09e934ba1b598037412643ad6078d5
mcthouacbb <mcthouacbb@gmail.com>
2026-08-06 19:52:32 -0400

    Remove default VertexToFragment implementations

--- crates/rasterizer/src/pipeline/vertex_to_fragment.rs
+++ crates/rasterizer/src/pipeline/vertex_to_fragment.rs
@@ -1,63 +1,7 @@
-use cgmath::{Vector2, Vector3, Vector4};
+use cgmath::Vector3;
 
 pub trait VertexToFragment: Copy + Sync + Send {
     fn scale_w(&mut self, scale: f32);
     fn interpolate2(a: &Self, b: &Self, t: f32) -> Self;
     fn interpolate3(a: &Self, b: &Self, c: &Self, barycentric: Vector3<f32>) -> Self;
-}
-
-impl VertexToFragment for f32 {
-    fn scale_w(&mut self, scale: f32) {
-        *self *= scale;
-    }
-
-    fn interpolate2(a: &Self, b: &Self, t: f32) -> Self {
-        a * (1.0 - t) + b * t
-    }
-
-    fn interpolate3(a: &Self, b: &Self, c: &Self, barycentric: Vector3<f32>) -> Self {
-        *a * barycentric.x + *b * barycentric.y + *c * barycentric.z
-    }
-}
-
-impl VertexToFragment for Vector2<f32> {
-    fn scale_w(&mut self, scale: f32) {
-        *self *= scale;
-    }
-
-    fn interpolate2(a: &Self, b: &Self, t: f32) -> Self {
-        a * (1.0 - t) + b * t
-    }
-
-    fn interpolate3(a: &Self, b: &Self, c: &Self, barycentric: Vector3<f32>) -> Self {
-        a * barycentric.x + b * barycentric.y + c * barycentric.z
-    }
-}
-
-impl VertexToFragment for Vector3<f32> {
-    fn scale_w(&mut self, scale: f32) {
-        *self *= scale;
-    }
-
-    fn interpolate2(a: &Self, b: &Self, t: f32) -> Self {
-        a * (1.0 - t) + b * t
-    }
-
-    fn interpolate3(a: &Self, b: &Self, c: &Self, barycentric: Vector3<f32>) -> Self {
-        a * barycentric.x + b * barycentric.y + c * barycentric.z
-    }
-}
-
-impl VertexToFragment for Vector4<f32> {
-    fn scale_w(&mut self, scale: f32) {
-        *self *= scale;
-    }
-
-    fn interpolate2(a: &Self, b: &Self, t: f32) -> Self {
-        a * (1.0 - t) + b * t
-    }
-
-    fn interpolate3(a: &Self, b: &Self, c: &Self, barycentric: Vector3<f32>) -> Self {
-        a * barycentric.x + b * barycentric.y + c * barycentric.z
-    }
 }
